21a210d901ead00f141b0d368f10bcc31d6657d0
RafaelaSouza0 <127880973+RafaelaSouza0@users.noreply.github.com>
2024-08-29 21:35:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,155 @@
+export const perguntas = [
+    {
+        enunciado: "Você está prestes a finalizar o ensino médio e se depara com diversas oportunidades para o futuro, como universidades, cursos ou empregos. Qual é o seu primeiro pensamento sobre como escolher o melhor caminho?",
+        alternativas: [
+            {
+                texto: "Preciso refletir profundamente sobre meus interesses e objetivos a longo prazo para tomar a melhor decisão.",
+                afirmacao: [
+                    "Penso sobre as disciplinas que mais gosto na escola e tento visualizar como essas se conectam com possíveis carreiras.",
+                    "Considero o que me traria mais satisfação pessoal a longo prazo, mesmo que seja algo menos convencional.",
+                    "Analiso os requisitos de diferentes carreiras e cursos para entender o que seria necessário alcançar."
+                ],
+                proxima: 1
+            },
+            {
+                texto: "Consulto um orientador profissional para obter uma perspectiva mais objetiva sobre minhas escolhas.",
+                afirmacao: [
+                    "Marcar uma reunião com o orientador da escola para discutir minhas opções.",
+                    "Procuro serviços de orientação vocacional que oferecem testes de aptidão e interesse.",
+                    "Pesquiso conselhos de especialistas em carreiras online e livros sobre escolhas profissionais."
+                ],
+                proxima: 2
+            }
+        ]
+    },
+
+     {
+        enunciado: "Sua orientadora pede que você elabore um plano de ação para alcançar seus objetivos futuros, seja na educação ou na carreira. Como você decide estruturar esse plano?",
+        alternativas: [
+            {
+                texto: "Utilizo recursos disponíveis para criar um plano detalhado, definindo etapas claras e metas específicas.",
+                afirmacao: [
+                    "Defino metas de curto, médio e longo prazo para ter uma visão clara do meu progresso.",
+                    "Uso aplicativos de gerenciamento de projetos para acompanhar meu progresso e prazos.",
+                    "Consulto exemplos de planos de ação bem-sucedidos para modelar o meu próprio plano."
+                ],
+                proxima: 3
+            },
+            {
+                texto: "Elaboro o plano com base em minhas próprias reflexões, experiências e feedback de mentores e colegas para garantir que esteja alinhado com meus sonhos.",
+                afirmacao: [
+                    "Converso com mentores sobre minhas ideias e recebo feedback construtivo.",
+                    "Reflito sobre minhas experiências anteriores e como elas podem influenciar meus próximos passos.",
+                    "Troco ideias com colegas que têm objetivos similares para inspirar o meu plano."
+                ],
+                proxima: 4
+            },
+            {
+                texto: "Divido meus objetivos em metas de curto, médio e longo prazo para facilitar o acompanhamento do progresso.",
+                afirmacao: [
+                    "Estabeleço prazos realistas para cada meta e reviso regularmente.",
+                    "Prioritizo metas de curto prazo que podem ser alcançadas rapidamente para ganhar impulso.",
+                    "Ajusto minhas metas conforme avanço, garantindo que estou sempre no caminho certo."
+                ],
+                proxima: 5
+            }
+        ]
+    },
+    
+        {
+        enunciado: "Durante uma discussão com seus amigos sobre planos futuros, um tema importante surge: como equilibrar a realização de sonhos pessoais com as responsabilidades diárias. Como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Acredito que é possível encontrar um equilíbrio ao definir prioridades e ajustar minhas responsabilidades conforme necessário.",
+                afirmacao: [
+                    "Crio uma lista de prioridades diárias para garantir que estou avançando em direção aos meus sonhos.",
+                    "Aloco tempo específico para trabalhar nos meus sonhos, sem comprometer minhas responsabilidades.",
+                    "Reviso regularmente meu progresso para ajustar minhas prioridades quando necessário."
+                ],
+                proxima: 6
+            },
+            {
+                texto: "Estou preocupado com como as responsabilidades podem impactar a realização de meus sonhos e a necessidade de encontrar formas práticas de gerenciar esse equilíbrio.",
+                afirmacao: [
+                    "Considero contratar um coach ou mentor para ajudar a equilibrar minhas responsabilidades.",
+                    "Busco ferramentas de gerenciamento de tempo para otimizar meu dia a dia.",
+                    "Peço apoio a familiares ou amigos para aliviar algumas responsabilidades, se possível."
+                ],
+                proxima: 7
+            },
+            {
+                texto: "Penso que sacrificar alguns sonhos é inevitável, mas o importante é nunca perder de vista meus principais objetivos.",
+                afirmacao: [
+                    "Identifico quais sonhos são mais importantes para mim e foco neles.",
+                    "Aceito que algumas coisas podem ser deixadas de lado temporariamente, mas planejo retomá-las no futuro.",
+                    "Reflito sobre o que estou disposto a sacrificar para alcançar meus principais objetivos."
+                ],
+                proxima: 8
+            }
+        ]
+    },
+    {
+        enunciado: "Você precisa criar uma apresentação que resuma suas metas e planos para o futuro. Qual método você escolhe para criar essa apresentação?",
+        alternativas: [
+            {
+                texto: "Crio uma apresentação detalhada usando ferramentas tradicionais para refletir minha visão pessoal e meus objetivos.",
+                afirmacao: [
+                    "Uso o PowerPoint ou Google Slides para criar uma apresentação estruturada.",
+                    "Incluo gráficos e tabelas que refletem meu planejamento e objetivos.",
+                    "Acompanho a apresentação com uma narração que explique detalhadamente cada ponto."
+                ],
+                proxima: 9
+            },
+            {
+                texto: "Utilizo um software avançado para criar uma visualização atraente e clara das minhas metas e estratégias futuras.",
+                afirmacao: [
+                    "Uso softwares como Prezi ou Canva para criar uma apresentação visualmente atraente.",
+                    "Incorporo animações e transições para tornar a apresentação mais envolvente.",
+                    "Integro infográficos que destacam os pontos chave das minhas metas."
+                ],
+                proxima: 10
+            },
+            {
+                texto: "Faço uma apresentação minimalista, focando mais no conteúdo do que na aparência.",
+                afirmacao: [
+                    "Uso uma abordagem simples e direta, com menos texto e mais impacto visual.",
+                    "Opto por um design limpo, com foco em mensagens claras e concisas.",
+                    "Concentro-me em transmitir o conteúdo essencial sem distrações visuais."
+                ],
+                proxima: 11
+            }
+        ]
+    },
+    {
+        enunciado: "Você está trabalhando em um projeto de grupo sobre metas e planos futuros, e um membro do grupo usou uma fonte externa para obter informações. O resultado final é muito semelhante ao que foi gerado por essa fonte. Como você lida com isso?",
+        alternativas: [
+            {
+                texto: "Acredito que utilizar o conteúdo gerado é aceitável, desde que o projeto esteja bem estruturado e todos tenham contribuído de alguma forma.",
+                afirmacao: [
+                    "Verifico se todas as fontes externas foram devidamente citadas para evitar plágio.",
+                    "Discuto com o grupo sobre como podemos adicionar mais ideias originais ao projeto.",
+                    "Certifico-me de que cada membro contribuiu significativamente para o projeto."
+                ],
+
+            },
+            {
+                texto: "Reviso o projeto e adiciono minhas próprias ideias e contribuições para garantir que o resultado final reflita nosso esforço e perspectiva pessoal.",
+                afirmacao: [
+                    "Sugiro novas seções ou pontos a serem incluídos no projeto.",
+                    "Proponho uma revisão coletiva para garantir que todos contribuam com novas ideias.",
+                    "Faço uma pesquisa adicional para enriquecer o conteúdo com insights únicos."
+                ],
+
+            },
+            {
+                texto: "Sugiro ao grupo que revisemos o material para garantir que estamos sendo autênticos e evitando plágio.",
+                afirmacao: [
+                    "Reviso o material utilizado e comparo com o projeto final para identificar similaridades.",
+                    "Consulto o orientador ou professor sobre como podemos evitar problemas de plágio.",
+                    "Sugiro que reescrevamos partes do projeto para garantir originalidade."
+                ],
+
+            }
+        ]
+    }
+];
